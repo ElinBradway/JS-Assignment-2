497c61d1bcc547d30c46219c74ce7660b07d07f2
gitelinbradway <elinbradway@gmail.com>
2024-10-20 23:30:30 +0200

    if statment working correct

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-let price = prompt('enter a price');
-let price = Number(price);
-reduction
+let priceNumber;
+
+while (true) {
+    priceNumber = prompt('Please enter a number');
+    priceNumber = Number(priceNumber);
+    
+    if (!isNaN(priceNumber)) {
+        break;
+    } else {
+        console.log('Invalid input, please enter a number');
+    }
+}
+
+reducedPrice = priceNumber * 0.9;
+
+console.log('your reduced price is ', reducedPrice);
