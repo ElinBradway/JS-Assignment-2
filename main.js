b36cebce8c5bf7caef957963faff020412fdb2b4
gitelinbradway <elinbradway@gmail.com>
2024-10-23 00:20:16 +0200

    Assignment 3

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
-let priceNumber;
+let username;
+let password;
 
-while (true) {
-    priceNumber = prompt('Please enter a number');
-    priceNumber = Number(priceNumber);
-    
-    if (!isNaN(priceNumber)) {
-        break;
-    } else {
-        console.log('Invalid input, please enter a number');
+function isValidPassword(password, username) {
+
+    if (password.length < 8) {
+        return false;
     }
-}
+    if (password === username) {
+        return false;
+      }
+      if (/\s/.test(password)) {
+        return false;
+      }
+      return true;
+} 
 
-reducedPrice = priceNumber * 0.9;
+console.log(isValidPassword("validpassword", "user"));
+console.log(isValidPassword("valid password", "user"));
+console.log(isValidPassword("user", "user"));
 
-console.log('your reduced price is ', reducedPrice);
+
+
+
+
+
+
+//*let priceNumber;
+
+//while (true) {
+//    priceNumber = prompt('Please enter a number');
+//    priceNumber = Number(priceNumber);
+    
+//   if (!isNaN(priceNumber)) {
+//        break;
+//    } else {
+//        console.log('Invalid input, please enter a number');
+//    }
+//}
+
+// reducedPrice = priceNumber * 0.9;
+
+//console.log('your reduced price is ', reducedPrice); 
